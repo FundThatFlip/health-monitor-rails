12b4fc984e7a26c72dd713c87b805d9fb6f7d4f0
Erika-Barr <ebbbarr@gmail.com>
2023-03-26 00:28:44 -0500

    add provider level cache

--- lib/health_monitor/providers/resque.rb
+++ lib/health_monitor/providers/resque.rb
@@ -8,10 +8,26 @@ module HealthMonitor
     class ResqueException < StandardError; end
 
     class Resque < Base
+      class Configuration
+        def cache_interval=(value)
+          HealthMonitor::Providers::Resque.cache_interval value
+        end
+      end
+
       def check!
         ::Resque.info
       rescue Exception => e
         raise ResqueException.new(e.message)
+      end
+
+      private
+
+      class << self
+        private
+
+        def configuration_class
+          ::HealthMonitor::Providers::Resque::Configuration
+        end
       end
     end
   end
